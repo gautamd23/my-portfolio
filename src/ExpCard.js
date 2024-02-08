2d923b14530e57639cc6cc5d5ad5827adc73d189
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-02-08 15:00:34 +0530

    created experience section

---
+++ src/ExpCard.js
@@ -0,0 +1,74 @@
+import React from "react";
+import check from "./assets/checkmark.png";
+
+export default function ExpCard({ skills, length, start }) {
+  return (
+    <div className="py-8 px-14 border-2 border-black rounded-3xl">
+      <h1 className="font-bold text-3xl  text-gray-700 text-center pb-7 text-gray-700">
+        Frontend Development
+      </h1>
+      <div className="grid grid-cols-2 gap-10">
+        {skills.slice(start, length).map((s, key) => {
+          return (
+            <div className="flex items-center gap-2">
+              <img className="w-7 " src={check}></img>
+              <div><h2 className="font-bold text-xl">
+                {s.skill}
+                <br />
+              </h2>
+              <h3>{s.level}</h3>
+            </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <div className="grid grid-cols-2 gap-4">
+        <div className="flex items-center gap-2">
+          <img className="w-7 " src={check}></img>
+          <h2>
+            HTML
+            <br /> Experienced
+          </h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <img className="w-7 " src={check}></img>
+          <h2>
+            CSS
+            <br /> Experienced
+          </h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <img className="w-7 " src={check}></img>
+          <h2>
+            SASS
+            <br /> Intermediate
+          </h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <img className="w-7 " src={check}></img>
+          <h2>
+            JavaScript
+            <br /> Basic
+          </h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <img className="w-7 " src={check}></img>
+          <h2>
+            TypeScript
+            <br /> Basic
+          </h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <img className="w-7 " src={check}></img>
+          <h2>
+            Material
+            <br /> UI Intermediate
+          </h2>
+        </div>
+      </div> */
+}
--- src/Experience.js
+++ src/Experience.js
@@ -1,7 +1,21 @@
 import React from "react";
 import check from "./assets/checkmark.png";
+import ExpCard from "./ExpCard";
 
 export default function Experience() {
+  const skills = [
+    { skill: "HTML", level: "Experienced" },
+    { skill: "CSS", level: "Experienced" },
+    { skill: "Javascript", level: "Experienced" },
+    { skill: "SASS", level: "experienced" },
+    { skill: "JavaScript", level: "experienced" },
+    { skill: "TypeScript", level: "experienced" },
+    { skill: "Material UI", level: "experienced" },
+    { skill: "PostgreSQL", level: "experienced" },
+    { skill: "Node JS", level: "experienced" },
+    { skill: "Express JS", level: "experienced" },
+    { skill: "Git", level: "experienced" },
+  ];
   return (
     <div className="px-28 py-10">
       <div className="py-5 text-center">
@@ -9,86 +23,8 @@ export default function Experience() {
         <h1 className="text-5xl font-bold py-3">Experience</h1>
       </div>
       <div className="flex gap-8 justify-center ">
-        <div className="py-5 px-5 border-2 border-black rounded-3xl">
-          <h1 className="font-bold text-2xl text-gray-700 text-center pb-5">
-            Frontend Development
-          </h1>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="flex items-center gap-2">
-              <img className="w-7 " src={check}></img>
-              <h2>
-                HTML
-                <br /> Experienced
-              </h2>
-            </div>
-            <div className="flex items-center gap-2">
-              <img className="w-7 " src={check}></img>
-              <h2>
-                CSS
-                <br /> Experienced
-              </h2>
-            </div>
-            <div className="flex items-center gap-2">
-              <img className="w-7 " src={check}></img>
-              <h2>
-                SASS
-                <br /> Intermediate
-              </h2>
-            </div>
-            <div className="flex items-center gap-2">
-              <img className="w-7 " src={check}></img>
-              <h2>
-                JavaScript
-                <br /> Basic
-              </h2>
-            </div>
-            <div className="flex items-center gap-2">
-              <img className="w-7 " src={check}></img>
-              <h2>
-                TypeScript
-                <br /> Basic
-              </h2>
-            </div>
-            <div className="flex items-center gap-2">
-              <img className="w-7 " src={check}></img>
-              <h2>
-                Material
-                <br /> UI Intermediate
-              </h2>
-            </div>
-          </div>
-        </div>
-        <div className="py-5 px-5 border-2 border-black rounded-3xl">
-          <h1 className="font-bold text-2xl text-gray-700 text-center pb-5">
-            Frontend Development
-          </h1>
-          <ul className="grid grid-cols-2 gap-4">
-            <li>
-              HTML
-              <br /> Experienced
-            </li>
-            <li>
-              CSS
-              <br /> Experienced
-            </li>
-            <li>
-              SASS
-              <br /> Intermediate
-            </li>
-            <li>
-              JavaScript
-              <br /> Basic
-            </li>
-            <li>
-              TypeScript
-              <br /> Basic
-            </li>
-            <li>
-              Material
-              <br /> UI Intermediate
-            </li>
-          </ul>
-        </div>
+        <ExpCard skills={skills} length={6} start={0}/>
+        <ExpCard skills={skills} length={skills.length} start={7}/>
       </div>
     </div>
   );
